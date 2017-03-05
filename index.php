c652feb9a2581d004d3b15af1d20b82ef1062045
Andre Bernardes <andrebernardes.midias@gmail.com>
2017-03-04 22:41:01 -0400

    mudei o codigo

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 
-echo "Meu primeiro código PHP! <strong>Olá Mundo!</strong>";
+echo "Meu primeiro código PHP! <strong>Olá Mundo 123!</strong>";
 
+echo "Meu primeiro código PHP! <strong>Olá Udemy 123!</strong>";
 
  ?>
 
